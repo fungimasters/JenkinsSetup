04a5721362bdbf0c34719776163d060d482cd02e
Daniel Mihaies <daniel.mihaies@gmail.com>
2018-03-30 11:07:35 +0300

    Test commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 const del = require('del');
 var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync');
-const eslint = require('gulp-eslint');
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 var gutil = require('gulp-util');
+var sassLint = require('gulp-sass-lint');
+var sass = require('gulp-sass');
 
-gulp.task('watch', ['browser-Sync', 'sass'], function () {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+function handleError(error, level) {
+  gutil.log(error.message);
+    process.exit(1);
+}
+gulp.task('watch', ['browser-Sync'], function () {
+  gulp.watch('app/scss/**/*.scss', ['compile']);
   gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/core/*.ts', browserSync.reload);
+  gulp.watch('app/core/*.ts', ['compile']);
 });
 
-gulp.task('browser-Sync', ['compile','nodemon'], function () {
+gulp.task('browser-Sync', ['compile', 'nodemon'], function () {
   browserSync({
     proxy: "localhost:3000", // local node app address
     port: 5000, // use *different* port than above
@@ -22,10 +26,17 @@ gulp.task('browser-Sync', ['compile','nodemon'], function () {
   });
 });
 
-gulp.task('sass', function () {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass())
-    .pipe(gulp.dest('app/dist/css'))
+gulp.task('sass', ['clean'], function () {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sassLint({
+      options:
+      {
+        configFile: '.sass-lint.yml'
+      }
+    }).on('error', handleError))
+    .pipe(sassLint.format().on('error', handleError))
+    .pipe(sass().on('error', sass.logError).on('error', handleError))
+    .pipe(gulp.dest('app/dist/css/'))
     .pipe(browserSync.reload({
       stream: true
     }))
@@ -35,17 +46,11 @@ gulp.task('clean', function () {
   return del('app/dist/**/*');
 });
 
-function handleError(level, error) {
-  gutil.log(error.message);
-  if (isFatal(level)) {
-     process.exit(1);
-  }
-}
-gulp.task('compile', ['clean'], function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .on('error', handleError)
-        .js.pipe(gulp.dest("app/dist/js"));
+gulp.task('compile', ['clean','sass'], function () {
+  return tsProject.src()
+    .pipe(tsProject())
+    .on('error', handleError)
+    .js.pipe(gulp.dest("app/dist/js"));
 });
 
 gulp.task('nodemon', function (cb) {
